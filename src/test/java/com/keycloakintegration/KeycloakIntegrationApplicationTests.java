a9039590a664e2e683cc86658a37c93a22512045
kumar-magilhub <kumar.reddy@magilhub.com>
2024-06-19 14:20:51 +0530

    feat:
    added missing thing

--- src/test/java/com/keycloakintegration/KeycloakIntegrationApplicationTests.java
+++ src/test/java/com/keycloakintegration/KeycloakIntegrationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KeycloakIntegrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
